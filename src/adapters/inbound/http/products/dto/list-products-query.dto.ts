86095e5e5b828679a6bb6b112cfadab2af696468
Alexis Plettener Nguyen <alexispn@ext.inditex.com>
2026-04-07 01:31:42 +0100

    refactor: extract @TransformBoolean() decorator for query param coercion
    Encapsulate the string-to-boolean conversion workaround for
    class-transformer into a reusable decorator, simplifying the DTO.

--- src/adapters/inbound/http/products/dto/list-products-query.dto.ts
+++ src/adapters/inbound/http/products/dto/list-products-query.dto.ts
@@ -1,27 +1,10 @@
 import { Transform } from 'class-transformer';
 import { IsBoolean, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { TransformBoolean } from '../../transforms/transform-boolean';
 
 export class ListProductsQueryDto {
   @IsOptional()
-  @Transform(({ value }) => {
-    if (value === undefined) {
-      return undefined;
-    }
-
-    if (typeof value === 'boolean') {
-      return value;
-    }
-
-    if (value === 'true') {
-      return true;
-    }
-
-    if (value === 'false') {
-      return false;
-    }
-
-    return value;
-  })
+  @TransformBoolean()
   @IsBoolean()
   activeOnly?: boolean;
 
---
+++ src/adapters/inbound/http/transforms/transform-boolean.ts
@@ -0,0 +1,11 @@
+import { Transform } from 'class-transformer';
+
+export function TransformBoolean(): PropertyDecorator {
+  return Transform(({ value }) => {
+    if (value === undefined) return undefined;
+    if (typeof value === 'boolean') return value;
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return value;
+  });
+}
